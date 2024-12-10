48ab4ca07289143abb5321405207044181943d89
Parshant Vardhan <149082789+Parshant95@users.noreply.github.com>
2024-12-10 18:05:21 +0000

    variables

---
+++ javabasics/basicvar.js
@@ -0,0 +1,19 @@
+const accountId=875412
+let accountEmail = "Parshant@google.com"
+var accountPass = "98001"
+accountCity = "Jaipur"
+
+// accountId= 3
+
+accountEmail="parshat@.com"
+accountPass="4565"
+accountCity="hp"
+
+console.table([accountId, accountEmail, accountPass, accountCity]);
+/*
+preffer not top use the var in the js because it has problem in the scope define it take variable as the global scope 
+which can conflict with other variable >>>> so we use let keyword to declare var
+
+2. if a varibale is not declare it default value is undefined.
+*/
+console.log(accountId);
