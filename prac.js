274b3cbbf59202c8a478802b6ab0e4f2b595a4eb
Parshnatvardhna <parshantvardhan8@gmail.com>
2025-02-19 10:09:45 +0530

    parshant js

---
+++ prac.js
@@ -0,0 +1,11 @@
+function a(){
+    console.log("a is called!");
+}
+// this is fucntion statement 
+a();
+var b =function b(){   
+    console.log("b is called");
+}
+b();
+
+//  this is fucntion declaration
